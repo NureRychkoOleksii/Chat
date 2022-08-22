85169c3c60a863f023e04b0bfea2061221a9ab57
m00nler <91968598+m00nler@users.noreply.github.com>
2022-08-22 15:47:37 +0300

    Created chat using SignalR

---
+++ Chat/Chat/Chat/Server/Hubs/PrivateChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Server.Hubs;
+
+public class PrivateChatHub : Hub
+{
+    private Dictionary<string, string> users = new();
+    public override async Task OnConnectedAsync()
+    {
+        string username = Context.GetHttpContext().Request.Query["username"];
+        users.Add(Context.ConnectionId, username);
+        await AddMessage("", $"{username} connected!"); 
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        string username = Context.GetHttpContext().Request.Query["username"];;
+        await AddMessage(String.Empty, $"{username} disconnected!");
+    }
+
+    public async Task AddMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+    
+}
